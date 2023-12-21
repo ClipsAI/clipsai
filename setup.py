11342a9f62c36732263e923fede57c7da410a861
Armel Talla <armel@clipsai.com>
2023-12-21 10:09:47 -0600

    update setup code

--- setup.py
+++ setup.py
@@ -1,29 +1,31 @@
 from setuptools import setup
 
-setup(name='clip',
-      version='0.1',
-      description='A Python package for clipping video content',
-      url='https://github.com/ClipsAI/clip',
-      author='ClipsAI',
-      author_email='support@clipsai.com',
-      license='GPLv3',
-      packages=['clip'],
-      # we need to go through pyproject.toml and determine what dependencies we need
-      install_requires=[
-          'numpy',
-          'opencv-python',
-          'mediapipe',
-          'scipy',
-          'torch',
-          'nltk',
-          'magic',
-          'reportlab',
-          'whisperx@git+https://github.com/m-bain/whisperx.git',
-          'sentence-transformers',
-          'matplotlib',
-          'cv2',
-          'pynvml',
-          'scenedetect'
-          'facenet-pytorch',
-        ],
-      zip_safe=False)
+setup(
+    name='clipsai',
+    version='0.1',
+    description='A Python package for clipping video content',
+    url='https://github.com/ClipsAI/clip',
+    author='ClipsAI',
+    author_email='support@clipsai.com',
+    license='GPLv3',
+    packages=['clipsai'],
+    # we need to go through pyproject.toml and determine what dependencies we need
+    install_requires=[
+        'numpy',
+        'opencv-python',
+        'mediapipe',
+        'scipy',
+        'torch',
+        'nltk',
+        'magic',
+        'reportlab',
+        'whisperx@git+https://github.com/m-bain/whisperx.git',
+        'sentence-transformers',
+        'matplotlib',
+        'cv2',
+        'pynvml',
+        'scenedetect'
+        'facenet-pytorch',
+    ],
+    zip_safe=False
+)
